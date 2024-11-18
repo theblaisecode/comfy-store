406d8d4f263ee026a73fa1de7341af279331b9b8
theblaisecode <theblaisecode@gmail.com>
2024-11-18 17:01:46 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -26,61 +26,64 @@ const queryClient = new QueryClient({
   },
 });
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <GlobalLayout />,
-    errorElement: <Error />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-        loader: homeLoader(queryClient),
-      },
-      {
-        path: "about",
-        element: <About />,
-      },
-      {
-        path: "products",
-        element: <Products />,
-        loader: productLoader(queryClient),
-      },
-      {
-        path: "products/:id",
-        element: <SingleProduct />,
-        loader: singleProductLoader(queryClient),
-      },
-      {
-        path: "cart",
-        element: <Cart />,
-      },
-      {
-        path: "checkout",
-        element: <Checkout />,
-        loader: checkoutLoader(store),
-        action: checkoutFormAction(store, queryClient),
-      },
-      {
-        path: "orders",
-        element: <Orders />,
-        loader: orderLoader(store, queryClient),
-      },
-    ],
-  },
-  {
-    path: "/login",
-    element: <Login />,
-    errorElement: <Error />,
-    action: loginAction(store),
-  },
-  {
-    path: "/register",
-    element: <Register />,
-    errorElement: <Error />,
-    action: registerAction,
-  },
-]{ basename: "/comfy-store" });
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <GlobalLayout />,
+      errorElement: <Error />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+          loader: homeLoader(queryClient),
+        },
+        {
+          path: "about",
+          element: <About />,
+        },
+        {
+          path: "products",
+          element: <Products />,
+          loader: productLoader(queryClient),
+        },
+        {
+          path: "products/:id",
+          element: <SingleProduct />,
+          loader: singleProductLoader(queryClient),
+        },
+        {
+          path: "cart",
+          element: <Cart />,
+        },
+        {
+          path: "checkout",
+          element: <Checkout />,
+          loader: checkoutLoader(store),
+          action: checkoutFormAction(store, queryClient),
+        },
+        {
+          path: "orders",
+          element: <Orders />,
+          loader: orderLoader(store, queryClient),
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+      errorElement: <Error />,
+      action: loginAction(store),
+    },
+    {
+      path: "/register",
+      element: <Register />,
+      errorElement: <Error />,
+      action: registerAction,
+    },
+  ],
+  { basename: "/comfy-store" }
+);
 
 function App() {
   return (
