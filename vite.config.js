7f0ae188df29f14cc168085e787bfbad4348ef71
theblaisecode <theblaisecode@gmail.com>
2024-11-18 17:05:39 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/comfy-store",
+  base: "/comfy-store",,
+  server: {
+    historyApiFallback: true,
+  },
 });
